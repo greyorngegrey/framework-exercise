346b714a04c758f4f5a192dc151ba3ad05e04d52
Julio Lopez <julio@hmlet.com>
2022-03-22 14:48:06 -0700

    - Added first networking API retrieving data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -7,8 +7,28 @@
 
 import Foundation
 
+
 extension RazeCore {
     public class Networking {
-         
+        
+        /// Responsible for handling all network calls
+        /// - Warning: Must create before using any public APIs
+        public class Manager {
+            public init() {}
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) { data, response, error in
+                    
+                }
+                task.resume()
+            }
+            
+            public enum NetworkResult<Value> {
+                case success(Value)
+                case failure(Error?)
+            }
+        }
     }
 }
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -6,9 +6,24 @@
 //
 
 import XCTest
+@testable import RazeCore
 
 final class RazeNetworkingTests: XCTestCase {
-    func testExample() throws {
-        
+    func testLoadDataCall() {
+        let manager = RazeCore.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        guard let url = URL(string: "https://www.reddit.com") else {
+            return XCTFail("Could not create url properly")
+        }
+        manager.loadData(from: url) { result in
+            expectation.fulfill()
+            switch result {
+                case .success(let returnedData):
+                XCTAssertNotNil(returnedData, "Response data is not nil.")
+                case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "Error forming result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
     }
 }
